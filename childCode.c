a55b3fb47f441a0eab99592720dc0d1f63912d4c
Tobias Brandy <tobiasbrandy@hotmail.com>
2020-03-23 11:08:07 -0300

    Retoques a lo de anoche

--- childCode.c
+++ childCode.c
@@ -14,71 +14,35 @@
 #include <limits.h>
 
 #define INPUT_MAX_SIZE 1000
-#define OUTPUT_MAX_SIZE 1000
+#define COMMAND_MAX_SIZE 1000
 #define PIPE_WRITE 1
 #define PIPE_READ 0
-#define MINISAT_PATH "/usr/bin/minisat"
 
 int main()
 {
-    setvbuf(stdout, NULL, _IONBF, 0);
+
     //Por stdin debe venir los path a los archivos minisat que le da master
-    //Por stdout devuelve la info que consiguio de minisat
-    char buff[INPUT_MAX_SIZE];
-    //int fdPipe[2];
-    //int forkId;
-    int aux;
+    //Por stdout devuelve la info que consiguio de minisat parseada
+
+    setvbuf(stdout, NULL, _IONBF, 0);
+
+    char input[INPUT_MAX_SIZE];
+    char command[COMMAND_MAX_SIZE];
+    size_t commandLen = 0;
     
-    if((aux = read(STDIN_FILENO, buff, INPUT_MAX_SIZE)) == -1) //Puede aux ser 0?
+    if((read(STDIN_FILENO, input, INPUT_MAX_SIZE)) == -1) //Puede aux ser 0?
         perror("FALLO EL READ");
-
-    if(aux > 0 && aux < INPUT_MAX_SIZE)
-        buff[aux] = 0;
     
-    // if(pipe(fdPipe)){
-    //     perror("PIPE INPUT ERROR:");
-    //     exit(EXIT_FAILURE);
-    // }
-
-    // if((forkId = fork()) == -1){
-    //     perror("FORK ERROR");
-    //     exit(EXIT_FAILURE);
-    // }
-
-    // if(!forkId){
-    //     close(fdPipe[PIPE_READ]);
-
-    //     dup2(fdPipe[PIPE_WRITE], STDOUT_FILENO);
-        
-    //     close(fdPipe[PIPE_WRITE]);
-
-    //     execl(MINISAT_PATH, "minisat", buff, (char*)NULL);
-    //     perror("EXEC OF CHILD FAILED");
-    // }
-
-    // close(fdPipe[PIPE_WRITE]);
-    size_t outputLen = 0;
-    char command[OUTPUT_MAX_SIZE];
-    sprintf(command, "minisat %s | grep -o -e \"Number of .*[0-9]\\+\" -e \"CPU time.*\" -e \".*SATISFIABLE\"", buff);
+    sprintf(command, "minisat %s | grep -o -e \"Number of .*[0-9]\\+\" -e \"CPU time.*\" -e \".*SATISFIABLE\"", input);
 
     FILE* fp = popen(command, "r"); //Validar
 
-    outputLen = fread(command, sizeof(char), OUTPUT_MAX_SIZE, fp); //Validar que sea EOF o error.
-    command[outputLen] = 0;
-    printf("%s", command);
+    commandLen = fread(command, sizeof(char), COMMAND_MAX_SIZE, fp); //Validar que sea EOF o error.
+    command[commandLen] = 0;
 
+    printf("File name:  %s\n%sID of slave who processed it:  %d", input, command, getpid());
 
-    // if((aux = read(fdPipe[PIPE_READ], buff, BUFFER_SIZE)) == -1) //Puede aux ser 0?
-    //     perror("FALLO EL READ");
-
-    // if(aux > 0 && aux < BUFFER_SIZE)
-    //     buff[aux] = 0;
-
-
-    //write(STDOUT_FILENO, buff, MAX_INPUT);
-    //printf("%s", buff);
-
-    //wait(NULL); //Validar el wait
+    wait(NULL); //Validar el wait
 
     return 0;
 }
