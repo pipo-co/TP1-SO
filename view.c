a5367eb732641729d8872e8f4b7174b30a09717b
Tobias Brandy <tobiasbrandy@hotmail.com>
2020-03-31 20:08:05 -0300

    Ahora funciona munmap

--- view.c
+++ view.c
@@ -55,23 +55,25 @@ int main(int argc, char const *argv[]){
         if(close(shm) == -1)
             perror("View - Close Shm");
 
-        sem_t * sm_sem = sem_open(SM_NAME, O_CREAT,O_RDWR,1);
+        sem_t * sm_sem = sem_open(SM_NAME, O_CREAT, O_RDWR,1);
         if(sm_sem == SEM_FAILED)
             HANDLE_ERROR("View - Open Semaphore sm_sem");
 
         sem_t * coms_sem = sem_open(COMS_NAME, O_CREAT, O_RDWR, 0);
         if(coms_sem == SEM_FAILED)
             HANDLE_ERROR("View - Open Semaphore coms_sem");
+    
+    char* mapIter = map;
 
     //Main process
-    while(viewProcess(&map, sm_sem, coms_sem));
+    while(viewProcess(&mapIter, sm_sem, coms_sem));
 
 
     freeResources(sm_sem, coms_sem, map, totalTasks);
     return 0;
 }
 
-int viewProcess(char ** map, sem_t *sm_sem, sem_t * coms_sem){
+int viewProcess(char ** mapIter, sem_t *sm_sem, sem_t * coms_sem){
 
     if(sem_wait(coms_sem) == -1)
         HANDLE_ERROR("View - Wait Sempahore coms_sem");
@@ -80,10 +82,10 @@ int viewProcess(char ** map, sem_t *sm_sem, sem_t * coms_sem){
     if(sem_wait(sm_sem) == -1)
         HANDLE_ERROR("View - Wait Sempahore sm_sem");
 
-    if(**map != 0){
+    if(**mapIter != 0){
 
-        printf("%s", *map);
-        *map += strlen(*map);
+        printf("%s", *mapIter);
+        *mapIter += strlen(*mapIter);
 
         if(sem_post(sm_sem) == -1)
             HANDLE_ERROR("View - Post Sempahore sm_sem");
